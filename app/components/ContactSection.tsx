2e49d9bbc9f182a0b5e05c0afe16637057d8a391
LeMax95 <maxim.leconiuc@gmail.com>
2025-08-13 00:26:20 +0300

    Initial commit - Lecobyte portfolio

--- app/components/ContactSection.tsx
+++ app/components/ContactSection.tsx
@@ -19,7 +19,7 @@ export const ContactSection = () => {
 						/>
 						<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
 					</svg>
-					<span>San Francisco, CA</span>
+					<span>Los Angeles, CA</span>
 				</div>
 
 				{/* Contact Buttons */}

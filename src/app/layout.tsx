81c01cdea7ae47e3a2e8e8e064dd12579aa5bdd5
LeMax95 <maxim.leconiuc@gmail.com>
2026-05-10 13:57:08 +0300

    added clientas

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,14 +12,16 @@ export const metadata: Metadata = {
   metadataBase: new URL(
     process.env.NEXT_PUBLIC_SITE_URL?.replace(/\/+$/, "") || "https://lecobyte.com"
   ),
+
   applicationName: "Lecobyte",
 
   icons: {
     icon: [
       { url: "/favicon.ico" },
-      { url: "/favicon-96x96.png", type: "image/png", sizes: "96x96" },
-      { url: "/icon.svg", type: "image/svg+xml" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
     ],
+
     apple: "/apple-touch-icon.png",
   },
 
